6ce7786e7d6458c8427cd3f9be9ae373af61bf58
Donnesca <163658888+Donnesca@users.noreply.github.com>
2025-03-13 17:13:24 +0200

    completed the first two Unresolved Stories

--- scripts.js
+++ scripts.js
@@ -5,5 +5,15 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  //result.innerText = dividend / divider;
+
+  const divisionResult = dividend / divider;
+  const flooredResult = Math.floor(divisionResult);
+  result.innerText = flooredResult;
+
+  if (!dividend || !divider) {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
 });
