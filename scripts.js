d51befb44c10d051ad5593e92376559f1c9713e0
Donnesca <163658888+Donnesca@users.noreply.github.com>
2025-03-14 11:29:08 +0200

    completed last two unresolved stories

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  //result.innerText = dividend / divider;
 
   const divisionResult = dividend / divider;
   const flooredResult = Math.floor(divisionResult);
@@ -14,6 +13,27 @@ form.addEventListener("submit", (event) => {
   if (!dividend || !divider) {
     result.innerText =
       "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+
+  const numDividend = parseFloat(dividend);
+  const numDivider = parseFloat(divider);
+
+  if (numDivider === 0) {
+    result.innerText =
+      "Division not performed. Invalid number provided. Try again.";
+    console.error(
+      "Division not performed. Invalid number provided. Try again",
+      new Error().stack
+    );
+    return;
+  }
+
+  if (isNaN(numDividend) || isNaN(numDivider)) {
+    const errorMessage =
+      "Something critical went wrong. Please reload the page.";
+    document.body.innerHTML = `<p style="color: red;">${errorMessage}</p>`;
+    console.error(errorMessage, new Error().stack);
     return;
   }
 });
